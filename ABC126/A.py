4cffdd3a0192fc0c69a9bb444db4d994eedbb8b7
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-05-21 01:54:44 +0900

    refactor A.py

--- ABC126/A.py
+++ ABC126/A.py
@@ -2,10 +2,34 @@
 https://atcoder.jp/contests/abc126/tasks/abc126_a
 '''
 
-N, K = map(int, input().split())
-S = list(input())
 
-S[K-1] = S[K-1].lower()
-S = ''.join(S)
+def get_in_map():
+    return map(int, input().split())
 
-print(S)
+
+def get_string():
+    return input()
+
+
+def main():
+    N, K = get_in_map()
+    S = list(get_string)
+
+    S[K-1] = S[K-1].lower()
+    S = ''.join(S)
+
+    print(S)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# 改修前
+# N, K = map(int, input().split())
+# S = list(input())
+
+# S[K-1] = S[K-1].lower()
+# S = ''.join(S)
+
+# print(S)
