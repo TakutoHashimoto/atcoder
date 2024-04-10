9d5a4b2539f2ed36f8bb55119b1925bdc1e330b6
TakutoHashimoto <tkthsmt0531@gmail.com>
2024-04-10 12:05:30 +0900

    Add

---
+++ abc/abc127/abc127_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    if (6 <= A and A <= 12) {
+        B /= 2;
+    } else if (A <= 5) {
+        B = 0;
+    }
+
+    cout << B << endl;
+}
